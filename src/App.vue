1534db196c70923628f668836451b78ed7ad3c56
Antonio Stassi <stassiant@outlook.it>
2024-11-17 02:21:52 +0100

    indentation tab 4

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import Login from './components/Login.vue'
+	import HelloWorld from "./components/HelloWorld.vue";
+	import Login from "./components/Login.vue";
 </script>
 
 <template>
-  <Login/>
+    <Login />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
